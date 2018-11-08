eea86cb638494e07e9f1fe7e5b2210f9523bf1f7
rodney salvador <rodneysalvador1106@gmail.com>
2018-11-08 18:29:59 +0800

    For User-Masterlist

--- application/controllers/admin/UserController.php
+++ application/controllers/admin/UserController.php
@@ -4,17 +4,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class UserMasterlistController extends Admin_Controller {
 
 
-	public function UserMasterlist()
-	{
-		$layout = array('charts' => TRUE, );
-		$this->load->view('layout/admin/1_css',$layout);
+		public function UserMasterlist() {
+		$this->load->model('admin/UserMasterlistModel','usermasterlistmodel');
+		$data['usermasterlist'] = $this->usermasterlistmodel->Load_UserMasterlistModel_Masterlist();
+		$layout = array('tables' => TRUE, );
+		$this->load->view('layout/admin/1_css');
 		$this->load->view('layout/admin/2_preloader');
 		$this->load->view('layout/admin/3_topbar');
 		$this->load->view('layout/admin/4_leftsidebar');
-		$this->load->view('pages/users/UserMasterlist');
-		$this->load->view('layout/admin/6_js',$layout);	
-		
+		$this->load->view('pages/users/UserMasterlist',$data);
+		$this->load->view('layout/admin/6_js',$layout);		
 	}
 
-
+	
 }
---
+++ application/models/admin/UserModel.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+	class UserModel extends Admin_Model {
+
+		public function Load_UserMasterlistModel_Masterlist() {
+			$this->db->select('*');
+			$this->db->from('tbl_security_users');
+			return $this->db->get();
+		}
+
+	}
