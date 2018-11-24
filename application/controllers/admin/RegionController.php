3a5f5cbea1152963be31724425bf92e8247112fe
Marvin Simplicio <marvin.simplicio@gmail.com>
2018-11-24 18:04:57 +0800

    3 Masterlist

---
+++ application/controllers/admin/RegionController.php
@@ -0,0 +1,118 @@
+ <?php
+ defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+ class RegionController extends CI_Controller {
+ 
+    function __construct() {
+         parent::__construct();
+         $this->load->model('admin/RegionModel','regmod');
+     }
+ 
+    public function Region()
+    {
+ 
+        $layout = array('tables'=>TRUE, 'datepicker'=>TRUE);
+        $data['masterlist'] = $this->regmod->LoadMasterlist();
+        $data['class'] = 'region';
+        $this->load->view('layout/admin/1_css');
+        $this->load->view('layout/admin/2_preloader');
+        $this->load->view('layout/admin/3_topbar');
+        $this->load->view('layout/admin/4_leftsidebar');
+        $this->load->view('pages/maintenance/Region',$data);
+        $this->load->view('layout/admin/6_js',$layout);     
+        $this->load->view('layout/admin/7_modals'); 
+
+    }
+    public function Create() {
+        $this->form_validation->set_rules('name','Name','required|is_unique[tbl_applicants_region.name]',
+                array(
+                'required'      => 'You have not provided %s.',
+                'is_unique'     => 'This %s already exists.'
+                )
+            );
+
+            if ($this->form_validation->run() == FALSE){
+             $errors = validation_errors();
+             echo json_encode(['error'=>$errors]);
+         }
+        else {
+            $postdata = $this->input->post();
+            $inserted = $this->regmod->Add($postdata);
+            // echo json_encode(['success'=>TRUE]);
+            if ($inserted != FALSE) {
+                $json = json_encode($inserted);             
+                echo $json;
+            }
+            else {
+                echo json_encode(['error'=>'Update Unsuccessful.']);
+            }
+         }
+ 
+    }
+ 
+    public function Update() {
+         $this->form_validation->set_rules('itemid', 'Item Record', 'required',
+                array(
+                'required'      => 'Cannot identify this record.',
+                ));
+
+        $postdata = $this->input->post();
+        if ($this->form_validation->run() == FALSE){
+            $errors = validation_errors();
+            echo json_encode(['error'=>$errors]);
+        }
+        else{
+            $id = $postdata['itemid'];
+            unset($postdata['itemid']);
+            $postdata = array_filter($postdata, 'strlen');
+
+            $result = $this->regmod->Update($id,$postdata);
+            if ($result != FALSE) {
+                $json = json_encode($result);             
+                echo $json;
+            }
+            else {
+                echo json_encode(['error'=>'Update Unsuccessful.']);
+            }
+        }
+
+
+
+
+    }
+ 
+    public function Delete() {
+ 
+         $this->form_validation->set_rules('id', 'Item Record', 'required',
+                array(
+                'required'      => 'Cannot identify this record.',
+                ));
+
+        $postdata = $this->input->post();
+        if ($this->form_validation->run() == FALSE){
+            $errors = validation_errors();
+            echo json_encode(['error'=>$errors]);
+        }
+        else{
+            $result = $this->regmod->Delete($postdata);
+            if ($result != FALSE) {
+                $json = json_encode($result);              
+                echo $json;
+            }
+            else {
+                echo json_encode(['error'=>'Update Unsuccessful.']);
+            }
+
+        }
+
+
+
+    }
+ 
+    public function Read() {
+ 
+    }
+ 
+ 
+ 
+ }
