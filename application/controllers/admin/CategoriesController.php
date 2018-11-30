965e401545b383264be91f36cabf14f1b3b708f8
John Perry Carpio <isystem20@gmail.com>
2018-11-30 13:28:50 +0800

    fixed

--- application/controllers/admin/CategoriesController.php
+++ application/controllers/admin/CategoriesController.php
@@ -1,7 +1,7 @@
  <?php
  defined('BASEPATH') OR exit('No direct script access allowed');
  
- class CategoriesController extends CI_Controller {
+ class CategoriesController extends Admin_Controller {
  
  	function __construct() {
          parent::__construct();
