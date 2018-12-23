2dc07688f5b7d21a397806f314b8258cadcfe14d
John Perry Carpio <isystem20@gmail.com>
2018-12-23 15:33:13 +0800

    fixed maintenance tables

--- application/views/pages/maintenance/ApplicantMasterlist.php
+++ application/views/pages/maintenance/ApplicantMasterlist.php
@@ -77,15 +77,7 @@
 
                                                      <button class="del-item-btn btn btn-danger waves-effect waves-light btn-sm" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete" type="button" data-action="<?=base_url('admin/'.$class.'/'); ?>" data-id="<?php echo $row->Id; ?>" data-name="<?=$row->lastName; ?>" > <i class="fas fa-trash-alt"></i></button>                                                  
                                                 </td>
-                                                <thead>
-                                            <tr>
-                                                <th>Lastname</th>
-                                                <th>Firstname</th>
-                                                <th>Email Address</th>
-                                                <th>Status</th>
-                                                <th>Action</th>
-                                            </tr>
-                                        </thead>
+                                   ]
                                             </tr>
                                         <?php
                                             }
