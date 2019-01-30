78a17dbf42ae138d2e45d7eb3f693e7c87d4ab49
John Perry Carpio <isystem20@gmail.com>
2019-01-30 21:47:48 +0800

    fixed

--- application/controllers/applicant/ApplicantBrowseJobsController.php
+++ application/controllers/applicant/ApplicantBrowseJobsController.php
@@ -1,7 +1,7 @@
  <?php
  defined('BASEPATH') OR exit('No direct script access allowed');
  
- class ApplicantBrowseJobsController extends CI_Controller {
+ class ApplicantBrowseJobsController extends Applicant_Controller {
  
     
  
